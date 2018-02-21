bd8e1589a87eae44a5941c24559f05b45beb4690
Aaron Magid <ahm64@case.edu>
2018-02-21 00:58:33 -0500

    Added testing file with empty tests.

---
+++ tests.js
@@ -0,0 +1,27 @@
+const assert = require('chai').assert;
+const functions = require('./network').testing;
+const dictionary = require('./dictionaries/dictionary_full.json');
+
+describe('PreprocessDictionary', function () {
+    it('', function () {
+
+    });
+});
+
+describe('EditDistanceOf1', function () {
+    it('', function () {
+        
+    });
+});
+
+describe('ProcessNextWord', function () {
+    it('', function () {
+        
+    });
+});
+
+describe('CalculateNetworkSize', function () {
+    it('', function () {
+        
+    });
+});
